1ddb9e7a542f65f60e5a4f231575b805f1510e5f
danielkvist <danielkvist@protonmail.com>
2019-07-24 12:53:56 +0200

    add main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/danielkvist/beagle
 
 go 1.12
+
+require github.com/spf13/cobra v0.0.5
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/danielkvist/beagle/cmd"
+
+func main() {
+	cmd.Execute()
+}
