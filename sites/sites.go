f3ecb19452e63005bf965987feaa05245abc6293
danielkvist <danielkvist@protonmail.com>
2019-07-25 17:57:51 +0200

    add comments for documentation

--- sites/sites.go
+++ sites/sites.go
@@ -1,3 +1,5 @@
+// Package sites defines utilities to parse and manage
+// sites from a .csv file with a name,url format.
 package sites
 
 import (
@@ -8,15 +10,21 @@ import (
 	"strings"
 )
 
+// Site defines a site with a name and a URL.
 type Site struct {
 	Name string
 	URL  string
 }
 
+// ReplaceURL receives a piece of the URL that
+// has to be replaced by a new string.
 func (s *Site) ReplaceURL(old string, new string) {
 	s.URL = strings.Replace(s.URL, old, new, 1)
 }
 
+// Parse receives the name of a .csv file
+// from which to extrar a list of sites and add
+// them to the []*Site returned if no errors occurs.
 func Parse(file string) ([]*Site, error) {
 	f, err := os.Open(file)
 	if err != nil {
